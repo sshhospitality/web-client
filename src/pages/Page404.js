398b327d70ab4a6d4c8d0167e62c64e5e4d77e4c
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2024-05-14 15:33:38 +0530

    fix:env

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -22,7 +22,7 @@ export default function Page404() {
   return (
     <>
       <Helmet>
-        <title> 404 Page Not Found | Naivedyam Mess System </title>
+        <title> 404 Page Not Found | Digimess Mess System </title>
       </Helmet>
 
       <Container>
